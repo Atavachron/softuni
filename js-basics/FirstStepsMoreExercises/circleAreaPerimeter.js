dab2423fa0f59f078bd496b5fde17abf704375fb
Atavachron <37329317+Atavachron@users.noreply.github.com>
2022-10-29 12:29:52 +0400

    Calculate circle area and perimeter

---
+++ js-basics/FirstStepsMoreExercises/circleAreaPerimeter.js
@@ -0,0 +1,11 @@
+function calculateCircleAreaPerimeter(input) {
+    let r = Number(input[0]);
+
+    let area = Math.PI * Math.pow(r, 2);
+    let perimeter = Math.PI * r * 2;
+
+    console.log(area.toFixed(2));
+    console.log(perimeter.toFixed(2));
+}
+
+calculateCircleAreaPerimeter(['3']);
