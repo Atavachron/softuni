f55d8f4621cb7b0595e0642d64bf5bf54f1d1e9c
Atavachron <37329317+Atavachron@users.noreply.github.com>
2022-10-29 13:45:37 +0400

    Calculate vineyard wine output

---
+++ js-basics/ConditionalsMoreExercises/harvest.js
@@ -0,0 +1,34 @@
+function calculateWineOutput(input) {
+    let vineyardArea = Number(input[0]);
+    let grapesHarvestPerSqM = Number(input[1]);
+    let litresWineNeeded = Number(input[2]);
+    let workerCount = Number(input[3]);
+
+    let totalGrapesHarvest = vineyardArea * grapesHarvestPerSqM;
+    let grapesForWine = totalGrapesHarvest * 0.4;
+    let wineProduced = grapesForWine / 2.5;
+
+    let diff = Math.abs(litresWineNeeded - wineProduced);
+
+    if (litresWineNeeded > wineProduced) {
+        console.log(
+            `It will be a tough winter! More ${Math.floor(
+                diff
+            )} liters wine needed.`
+        );
+    } else {
+        console.log(
+            `Good harvest this year! Total wine: ${Math.floor(
+                wineProduced
+            )} liters.`
+        );
+        console.log(
+            `${Math.ceil(diff)} liters left -> ${Math.ceil(
+                diff / workerCount
+            )} liters per person.`
+        );
+    }
+}
+
+calculateWineOutput(['650', '2', '175', '3']);
+calculateWineOutput(['1020', '1.5', '425', '4']);
