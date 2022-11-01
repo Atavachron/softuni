1f4e04f94354b6f8651875176d2ed78cf2f37cae
Atavachron <37329317+Atavachron@users.noreply.github.com>
2022-11-01 22:14:54 +0400

    Log animal type depending on animal name

---
+++ js-basics/AdvancedConditionals/animalType.js
@@ -0,0 +1,21 @@
+function logAnimalType(input) {
+    let animal = input[0];
+
+    switch (animal) {
+        case 'dog':
+            console.log('mammal');
+            break;
+        case 'crocodile':
+        case 'tortoise':
+        case 'snake':
+            console.log('reptile');
+            break;
+        default:
+            console.log('unknown');
+            break;
+    }
+}
+
+logAnimalType(['dog']);
+logAnimalType(['snake']);
+logAnimalType(['cat']);
