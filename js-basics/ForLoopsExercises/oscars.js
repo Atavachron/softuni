8b030f7cb7a7752bb88078db606c24cd0cb2d4e5
Atavachron <37329317+Atavachron@users.noreply.github.com>
2022-11-16 23:56:30 +0400

    First commit in oscars.js

---
+++ js-basics/ForLoopsExercises/oscars.js
@@ -0,0 +1,19 @@
+function giveOscar(input) {
+    let name = input[0];
+    let academyPoits = Number(input[1]);
+    let judgeCount = Number(input[2]);
+}
+
+giveOscar([
+    'Zahari Baharov',
+    '205',
+    '4',
+    'Johnny Depp',
+    '45',
+    'Will Smith',
+    '29',
+    'Jet Lee',
+    '10',
+    'Matthew Mcconaughey',
+    '39',
+]);
