3c90e6ee8ba029660b61ea1e3b6d78cbca0d0e08
Atavachron <37329317+Atavachron@users.noreply.github.com>
2022-11-17 00:07:41 +0400

    Calculate if an actor gets Oscar nomination

--- js-basics/ForLoopsExercises/oscars.js
+++ js-basics/ForLoopsExercises/oscars.js
@@ -1,7 +1,31 @@
 function giveOscar(input) {
     let name = input[0];
-    let academyPoits = Number(input[1]);
-    let judgeCount = Number(input[2]);
+    let academyPoints = Number(input[1]);
+
+    let totalPoints = academyPoints;
+
+    for (let i = 3; i < input.length; i += 2) {
+        let judgeNameLength = Number(input[i].length);
+        let judgePoints = Number(input[i + 1]);
+        let pointsAwarded = (judgeNameLength * judgePoints) / 2;
+        totalPoints += pointsAwarded;
+
+        if (totalPoints > 1250.5) {
+            break;
+        }
+    }
+
+    if (totalPoints > 1250.5) {
+        console.log(
+            `Congratulations, ${name} got a nominee for leading role with ${totalPoints.toFixed(
+                1
+            )}!`
+        );
+    } else {
+        console.log(
+            `Sorry, ${name} you need ${(1250.5 - totalPoints).toFixed(1)} more!`
+        );
+    }
 }
 
 giveOscar([
@@ -16,4 +40,20 @@ giveOscar([
     '10',
     'Matthew Mcconaughey',
     '39',
+]);
+
+giveOscar([
+    'Sandra Bullock',
+    '340',
+    '5',
+    'Robert De Niro',
+    '50',
+    'Julia Roberts',
+    '40.5',
+    'Daniel Day-Lewis',
+    '39.4',
+    'Nicolas Cage',
+    '29.9',
+    'Stoyanka Mutafova',
+    '33',
 ]);
