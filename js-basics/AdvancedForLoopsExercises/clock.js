13f7551addd216d970ce2a5210c3fbafd235243e
Atavachron <37329317+Atavachron@users.noreply.github.com>
2022-11-19 00:47:13 +0400

    Log 24-hour clock with seconds

--- js-basics/AdvancedForLoopsExercises/clock.js
+++ js-basics/AdvancedForLoopsExercises/clock.js
@@ -1,7 +1,9 @@
 function logClock() {
     for (let i = 0; i < 24; i++) {
         for (let j = 0; j < 60; j++) {
-            console.log(`${i} : ${j}`);
+            for (let k = 0; k < 60; k++) {
+                console.log(`${i} : ${j} : ${k}`);
+            }
         }
     }
 }
