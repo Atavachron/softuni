d5e94112786509e872b3a9f7a41c73d988aa4a6c
Atavachron <37329317+Atavachron@users.noreply.github.com>
2022-11-22 01:05:47 +0400

    Sum numbers until target is reached

---
+++ js-basics/WhileLoops/sumNumbers.js
@@ -0,0 +1,19 @@
+function sumNumbers(input) {
+    let index = 0;
+
+    let targetSum = Number(input[index]);
+    index++;
+
+    let sum = 0;
+
+    while (sum < targetSum) {
+        let currentNum = Number(input[index]);
+        sum += currentNum;
+        index++;
+    }
+
+    console.log(sum);
+}
+
+sumNumbers(['100', '10', '20', '30', '40']);
+sumNumbers(['20', '1', '2', '3', '4', '5', '6']);
