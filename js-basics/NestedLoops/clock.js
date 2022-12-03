01642cec46a56e228d4ac72c52119d4f019ee61f
Atavachron <37329317+Atavachron@users.noreply.github.com>
2022-12-03 17:59:07 +0400

    Add leading zero to clock display

--- js-basics/NestedLoops/clock.js
+++ js-basics/NestedLoops/clock.js
@@ -1,7 +1,19 @@
 function displayClock() {
     for (let h = 0; h < 24; h++) {
         for (let m = 0; m < 60; m++) {
-            console.log(`${h}:${m}`);
+            if (h < 10) {
+                if (m < 10) {
+                    console.log(`0${h}:0${m}`);
+                } else {
+                    console.log(`0${h}:${m}`);
+                }
+            } else {
+                if (m < 10) {
+                    console.log(`${h}:0${m}`);
+                } else {
+                    console.log(`${h}:${m}`);
+                }
+            }
         }
     }
 }
