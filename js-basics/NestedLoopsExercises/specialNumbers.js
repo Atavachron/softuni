13493c97f625ced011c656fca36622ad9ff900b0
Atavachron <37329317+Atavachron@users.noreply.github.com>
2022-12-22 23:57:37 +0400

    Use nested loops to display a special number

--- js-basics/NestedLoopsExercises/specialNumbers.js
+++ js-basics/NestedLoopsExercises/specialNumbers.js
@@ -1,3 +1,19 @@
-function solve(input) {}
+function solve(input) {
+    let n = Number(input[0]);
+    let output = '';
+    for (let i = 1111; i <= 9999; i++) {
+        let specialNum = '';
+        let str = i.toString();
+        for (let j = 0; j < str.length; j++) {
+            if (n % Number(str[j]) === 0) {
+                specialNum += str[j];
+            }
+        }
+        if (specialNum.length === 4) {
+            output += specialNum + ' ';
+        }
+    }
+    console.log(output);
+}
 
-solve();
+solve(['3']);
