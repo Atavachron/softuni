b3c48b2e7b430fa0d5b962b5ea437c2fd1807831
Atavachron <37329317+Atavachron@users.noreply.github.com>
2022-12-31 00:07:22 +0400

    Fix for loop in Wedding Seats js

--- js-basics/NestedLoopsMoreExercises/weddingSeats.js
+++ js-basics/NestedLoopsMoreExercises/weddingSeats.js
@@ -6,29 +6,26 @@ function solve(input) {
 
     for (let i = 'A'.charCodeAt(0); i <= lastSector.charCodeAt(0); i++) {
         for (let j = 1; j <= firstSectorRows; j++) {
-            // if (j % 2 !== 0) {
-            //     for (let k = 'a'.charCodeAt(0); k <= oddRowSeats; k++) {
-            //         console.log(
-            //             `${String.fromCharCode(i)}${j}${String.fromCharCode(k)}`
-            //         );
-            //         totalSeats++;
-            //     }
-            // } else {
-            //     for (let k = 'a'.charCodeAt(0); k <= oddRowSeats + 2; k++) {
-            //         console.log(
-            //             `${String.fromCharCode(i)}${j}${String.fromCharCode(k)}`
-            //         );
-            //         totalSeats++;
-            //     }
-            // }
             if (j % 2 !== 0) {
-                for (let k = 1; k <= 3; k++) {
-                    console.log(`${String.fromCharCode(i)}${j}${k}`);
+                for (
+                    let k = 'a'.charCodeAt(0);
+                    k < 'a'.charCodeAt(0) + oddRowSeats;
+                    k++
+                ) {
+                    console.log(
+                        `${String.fromCharCode(i)}${j}${String.fromCharCode(k)}`
+                    );
                     totalSeats++;
                 }
             } else {
-                for (let k = 1; k <= 5; k++) {
-                    console.log(`${String.fromCharCode(i)}${j}${k}`);
+                for (
+                    let k = 'a'.charCodeAt(0);
+                    k < 'a'.charCodeAt(0) + oddRowSeats + 2;
+                    k++
+                ) {
+                    console.log(
+                        `${String.fromCharCode(i)}${j}${String.fromCharCode(k)}`
+                    );
                     totalSeats++;
                 }
             }
