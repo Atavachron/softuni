2c8f99d893eb2fdeedbd2d57eae8db0d20040326
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-01-15 00:13:20 +0400

    Check if a year is a leap year

---
+++ js-fundamentals/BasicSyntaxExercises/leapYear.js
@@ -0,0 +1,11 @@
+function checkIfLeapYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        console.log('yes');
+    } else {
+        console.log('no');
+    }
+}
+
+checkIfLeapYear(1984);
+checkIfLeapYear(2003);
+checkIfLeapYear(4);
