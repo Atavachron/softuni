4419597a5fe1e2dfbe48965e84a5862844cb1c20
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-01-17 02:05:37 +0400

    Refactor calculateBill function

--- js-basics/ExamMay2019/fruitMarket.js
+++ js-basics/ExamMay2019/fruitMarket.js
@@ -1,10 +1,10 @@
-function calculateBill(input) {
-    let strawberryPrice = Number(input[0]);
-    let bananas = Number(input[1]);
-    let oranges = Number(input[2]);
-    let raspberries = Number(input[3]);
-    let strawberries = Number(input[4]);
-
+function calculateBill(
+    strawberryPrice,
+    bananas,
+    oranges,
+    raspberries,
+    strawberries
+) {
     let raspberryPrice = strawberryPrice / 2;
     let orangePrice = raspberryPrice * 0.6;
     let bananaPrice = raspberryPrice * 0.2;
@@ -18,4 +18,4 @@ function calculateBill(input) {
     console.log(total.toFixed(2));
 }
 
-calculateBill(['48', '10', '3.3', '6.5', '1.7']);
+calculateBill(48, 10, 3.3, 6.5, 1.7);
