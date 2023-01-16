e4065f87bd650a66cb1338ae4ba1cd87b95652d4
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-01-17 01:04:04 +0400

    Calculate distance between two points in a coordinate system

---
+++ js-fundamentals/BasicSyntaxMoreExercises/distance.js
@@ -0,0 +1,9 @@
+function findDistance(x1, y1, x2, y2) {
+    let distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    console.log(distance);
+}
+
+findDistance(2, 4, 5, 0);
+findDistance(2.34, 15.66, -13.55, -2.9985);
+
+console.log(Math.pow(3, 2));
