7546bdee9c1002eaf9d5314ae8389b45f4af698b
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-01-19 19:35:02 +0400

    Determine if a num is amazing if digits sum includes 9

---
+++ js-fundamentals/DataTypesAndVariables/amazingNumbers.js
@@ -0,0 +1,14 @@
+function checkIfAmazing(num) {
+    num = num.toString();
+    let sum = 0;
+    for (let i = 0; i < num.length; i++) {
+        sum += Number(num[i]);
+    }
+
+    let result = sum.toString().includes('9');
+
+    console.log(result ? `${num} Amazing? True` : `${num} Amazing? False`);
+}
+
+checkIfAmazing(1233);
+checkIfAmazing(999);
