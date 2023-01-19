9d06801697831a0fd6d18636a87536a25f7fb33c
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-01-19 21:04:10 +0400

    Print triples of latin letters

---
+++ js-fundamentals/DataTypesAndVariables/triplesOfLatinLetters.js
@@ -0,0 +1,15 @@
+function printTriplesOfLetters(num) {
+    for (let i = 97; i < 97 + num; i++) {
+        for (let j = 97; j < 97 + num; j++) {
+            for (let k = 97; k < 97 + num; k++) {
+                console.log(
+                    `${String.fromCharCode(i)}${String.fromCharCode(
+                        j
+                    )}${String.fromCharCode(k)}`
+                );
+            }
+        }
+    }
+}
+
+printTriplesOfLetters(3);
