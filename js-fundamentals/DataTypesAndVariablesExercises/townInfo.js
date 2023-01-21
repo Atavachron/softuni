68d3b3bcfda6f77b120bbd8fa2410c7be28a92fd
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-01-21 14:52:53 +0400

    Print town info based on input data

---
+++ js-fundamentals/DataTypesAndVariablesExercises/townInfo.js
@@ -0,0 +1,6 @@
+function printTownInfo(town, population, area) {
+    let info = `Town ${town} has population of ${population} and area ${area} square km.`;
+    console.log(info);
+}
+
+printTownInfo('Sofia', 1286383, 492);
