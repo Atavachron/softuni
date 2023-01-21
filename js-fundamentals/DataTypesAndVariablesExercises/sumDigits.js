cec5d5cb9b797775ce5896ab86a029c082a5825a
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-01-21 14:46:40 +0400

    Find the sum of all digits of a given number

---
+++ js-fundamentals/DataTypesAndVariablesExercises/sumDigits.js
@@ -0,0 +1,13 @@
+function sumDigits(num) {
+    let numAsString = num.toString();
+    let sum = 0;
+
+    for (let i = 0; i < numAsString.length; i++) {
+        let digit = Number(numAsString[i]);
+        sum += digit;
+    }
+
+    console.log(sum);
+}
+
+sumDigits(97561);
