910e7ad6f7713b6319b4a8dd7f842ae6b41957c2
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-01-26 23:14:35 +0400

    Sum first and last array element

---
+++ js-fundamentals/Arrays/sumFirstAndLastElement.js
@@ -0,0 +1,9 @@
+function sumFirstAndLastElement(arr) {
+    let first = arr[0];
+    let last = arr[arr.length - 1];
+    let sum = first + last;
+
+    console.log(sum);
+}
+
+sumFirstAndLastElement([20, 30, 40]);
