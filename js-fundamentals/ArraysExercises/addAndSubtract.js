4b5a41dfa7e67fb970530ebf7e766539c55a2386
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-01-30 00:00:18 +0400

    Add or subtract index from element in array

--- js-fundamentals/ArraysExercises/addAndSubtract.js
+++ js-fundamentals/ArraysExercises/addAndSubtract.js
@@ -1,4 +1,26 @@
-function addAndSubtract(arr) {}
+function addAndSubtract(arr) {
+    let newArr = [];
+    let initialSum = 0;
+    let finalSum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        let element = Number(arr[i]);
+        initialSum += element;
+
+        if (element % 2 === 0) {
+            element += i;
+        } else {
+            element -= i;
+        }
+
+        newArr.push(element);
+        finalSum += element;
+    }
+
+    console.log(newArr);
+    console.log(initialSum);
+    console.log(finalSum);
+}
 
 addAndSubtract([5, 15, 23, 56, 35]);
 addAndSubtract([-5, 11, 3, 0, 2]);
