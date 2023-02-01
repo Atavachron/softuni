862901e2f8c0564f92c7c635b108dc04ad2321ca
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-02-02 01:14:25 +0400

    Print n-th element in an array

---
+++ js-fundamentals/ArraysMoreExercises/printNthElement.js
@@ -0,0 +1,14 @@
+function printNthStep(arr) {
+    let step = Number(arr[arr.length - 1]);
+    let collection = [];
+
+    for (let i = 0; i < arr.length - 1; i += step) {
+        collection.push(arr[i]);
+    }
+
+    console.log(collection.join(' '));
+}
+
+// printNthStep(['5', '20', '31', '4', '20', '2']);
+// printNthStep(['dsa', 'asd', 'test', 'test', '2']);
+printNthStep(['1', '2', '3', '4', '5', '6']);
