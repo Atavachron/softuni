494fa3b0a05358ede520ed255e329e5f611fe5f8
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-02-04 23:57:24 +0400

    Comments added

--- js-fundamentals/Functions/reverseArray.js
+++ js-fundamentals/Functions/reverseArray.js
@@ -1,10 +1,12 @@
 function reverseArray(arr) {
     for (let i = 0; i < arr.length / 2; i++) {
+        //Call the swap function the number of times equal to half the array
         swap(arr, i, (j = arr.length - 1 - i));
     }
 
     return arr.join(' ');
 
+    //Pass the swap function the array, and the indexes of the elements to be swapped
     function swap(arr, i, j) {
         let temp = arr[i];
         arr[i] = arr[j];
