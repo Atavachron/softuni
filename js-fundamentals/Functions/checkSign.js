5c195e001e0ae8479d391c99be7a58d5367ab783
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-02-05 02:11:59 +0400

    Refactor check sign js to accept any number of arguments

--- js-fundamentals/Functions/checkSign.js
+++ js-fundamentals/Functions/checkSign.js
@@ -1,17 +1,23 @@
 //Do not multiply the arguments to find out the answer
 
-function checkSign(numOne, numTwo, numThree) {
+function checkSign(...arguments) {
     let positiveCount = 0;
 
     for (let element of arguments) {
         element > 0 ? positiveCount++ : null;
     }
 
-    if (positiveCount === 1 || positiveCount === 3) {
-        return 'Positive';
+    if (arguments.length % 2 === 0) {
+        if (positiveCount % 2 === 0) {
+            return 'Positive';
+        }
+    } else {
+        if (positiveCount % 2 !== 0) {
+            return 'Positive';
+        }
     }
 
     return 'Negative';
 }
 
-console.log(checkSign(-1, -3, -4));
+console.log(checkSign(-1, -3, -4, -4, 6, 9, -4));
