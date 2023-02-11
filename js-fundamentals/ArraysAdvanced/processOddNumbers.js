b778c197e48883cbfea1206ae8f37cd8a9866043
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-02-11 05:18:22 +0400

    Remove join method from chained methods

--- js-fundamentals/ArraysAdvanced/processOddNumbers.js
+++ js-fundamentals/ArraysAdvanced/processOddNumbers.js
@@ -2,10 +2,9 @@ function processOddNumbers(input) {
     let newArr = input
         .filter((x, i) => i % 2 !== 0)
         .map(x => x * 2)
-        .reverse()
-        .join(' ');
+        .reverse();
 
-    return newArr;
+    return newArr.join(' ');
 }
 
 console.log(processOddNumbers([10, 15, 20, 25]));
