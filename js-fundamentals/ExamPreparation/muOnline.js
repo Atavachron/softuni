5843c8fb404516b8bae2cfd9b39c0a449c6704e3
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-02-16 01:31:35 +0400

    A version of Mu Online game

--- js-fundamentals/ExamPreparation/muOnline.js
+++ js-fundamentals/ExamPreparation/muOnline.js
@@ -1,16 +1,14 @@
-function playGame(input) {
+function muOnline(input) {
     let health = 100;
-    let coins = 0;
+    let bitcoins = 0;
     let isAlive = true;
 
-    let roomsArr = input.toString().split('|');
+    let commandsArray = input.split('|');
 
-    for (let i = 0; i < roomsArr.length; i++) {
-        let room = roomsArr[i];
-        let singleRoomArr = room.split(' ');
-
-        let command = singleRoomArr[0];
-        let points = Number(singleRoomArr[1]);
+    for (let i = 0; i < commandsArray.length; i++) {
+        let currentElement = commandsArray[i].split(' ');
+        let command = currentElement[0];
+        let points = Number(currentElement[1]);
 
         if (command === 'potion') {
             let healingPoints = 0;
@@ -25,8 +23,8 @@ function playGame(input) {
             console.log(`You healed for ${healingPoints} hp.`);
             console.log(`Current health: ${health} hp.`);
         } else if (command === 'chest') {
-            coins += points;
-            console.log(`You found ${points} coins.`);
+            bitcoins += points;
+            console.log(`You found ${points} bitcoins.`);
         } else {
             let monster = command;
             health -= points;
@@ -42,11 +40,11 @@ function playGame(input) {
     }
 
     if (isAlive) {
-        console.log(`You've made it!`);
-        console.log(`Coins: ${coins}`);
+        console.log("You've made it!");
+        console.log(`Bitcoins: ${bitcoins}`);
         console.log(`Health: ${health}`);
     }
 }
 
-// playGame(['rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000']);
-playGame(['cat 10|potion 30|orc 10|chest 10|snake 25|chest 110']);
+muOnline('rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000');
+muOnline('cat 10|potion 30|orc 10|chest 10|snake 25|chest 110');
