6e0aad59e314558cad256687123ccf20ebe48419
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-02-24 02:21:56 +0400

    Use destructuring and Object.entries to log key value pairs

---
+++ js-fundamentals/ObjectsAndClasses/city2.js
@@ -0,0 +1,13 @@
+function displayCityInfo(city) {
+    for (let [key, value] of Object.entries(city)) {
+        console.log(`${key} -> ${value}`);
+    }
+}
+
+displayCityInfo({
+    name: 'Plovdiv',
+    area: 389,
+    population: 1162358,
+    country: 'Bulgaria',
+    postCode: '4000',
+});
