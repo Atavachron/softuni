6fd91a9285979a823f11d72171de0c6996ded4e6
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-02-24 02:30:40 +0400

    Use destructuring

--- js-fundamentals/ObjectsAndClasses/convertToObject.js
+++ js-fundamentals/ObjectsAndClasses/convertToObject.js
@@ -1,7 +1,15 @@
+// function convertToObject(str) {
+//     let obj = JSON.parse(str);
+//     for (let key of Object.keys(obj)) {
+//         console.log(`${key}: ${obj[key]}`);
+//     }
+// }
+
 function convertToObject(str) {
     let obj = JSON.parse(str);
-    for (let key of Object.keys(obj)) {
-        console.log(`${key}: ${obj[key]}`);
+
+    for (let [key, value] of Object.entries(obj)) {
+        console.log(`${key}: ${value}`);
     }
 }
 
