e3bdaa97b41cc61853463f184b7a857f05c21e9e
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-02-24 17:39:03 +0400

    Remove repeating elments from an array

---
+++ js-fundamentals/ArraysAdvancedExercises/distinctArray.js
@@ -0,0 +1,20 @@
+// function removeRepeatingElements(arr) {
+//     let newArr = Array.from(new Set(arr));
+//     console.log(newArr);
+// }
+
+function removeRepeatingElements(input) {
+    let newArray = [];
+
+    for (let i = 0; i < input.length; i++) {
+        if (!newArray.includes(input[i])) {
+            newArray.push(input[i]);
+        }
+    }
+
+    console.log(newArray.join(' '));
+}
+
+removeRepeatingElements([1, 2, 3, 4]);
+removeRepeatingElements([7, 8, 9, 7, 2, 3, 4, 1, 2]);
+removeRepeatingElements([20, 8, 12, 13, 4, 4, 8, 5]);
