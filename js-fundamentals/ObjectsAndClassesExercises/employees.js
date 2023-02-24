387be1b02d7d2614c3201f0b820c8947820ddd42
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-02-25 03:13:40 +0400

    Create objects from input data and log info

---
+++ js-fundamentals/ObjectsAndClassesExercises/employees.js
@@ -0,0 +1,18 @@
+function createEmployeeList(input) {
+    let obj = {};
+
+    for (let name of input) {
+        obj[name] = name.length;
+    }
+
+    for (let key of Object.keys(obj)) {
+        console.log(`Name: ${key} -- Personal Number: ${obj[key]}`);
+    }
+}
+
+createEmployeeList([
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal',
+]);
