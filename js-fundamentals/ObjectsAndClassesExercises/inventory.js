2381c44cb2ccd2fe5afc4cb502ec6ad1ae88402d
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-02-25 23:33:14 +0400

    Add comments

--- js-fundamentals/ObjectsAndClassesExercises/inventory.js
+++ js-fundamentals/ObjectsAndClassesExercises/inventory.js
@@ -1,16 +1,21 @@
 function createHeroInventory(input) {
+    //Create empty array to store the hero objects
     let heroes = [];
 
+    //Iterate over each element of the input
     for (let el of input) {
+        //Use destructuring to get the name, level and items from each input entry
         let [name, level, items] = el.split(' / ');
 
+        //Create a her object
         let hero = { name, level, items };
 
+        //Push the hero object to the array
         heroes.push(hero);
     }
 
     heroes
-        .sort((a, b) => a.level - b.level)
+        .sort((a, b) => a.level - b.level) //Sort the hero objects, based on their level
         .forEach(hero => {
             console.log(`Hero: ${hero.name}`);
             console.log(`level => ${hero.level}`);
