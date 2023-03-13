3757ca0bf40769a5404e3bec4839e5c6caea25dc
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-03-13 10:24:42 +0400

    Comments added

--- js-fundamentals/TextProcessingExercises/replaceRepeatingChars.js
+++ js-fundamentals/TextProcessingExercises/replaceRepeatingChars.js
@@ -2,6 +2,8 @@ function replaceRepeatingChars(str) {
     let index = 1;
     let result = str[0];
 
+    //Check if the current char is the same like the previous one.
+    //If same, do nothing. If different, add the char the result
     while (index < str.length) {
         if (str[index] !== str[index - 1]) {
             result += str[index];
