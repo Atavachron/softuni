ad4f51d0138d2c554bdbcf8fa9294bf7615edcf7
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-03-27 12:40:58 +0400

    Use regexp in replace method to avoid border cases

--- js-fundamentals/FinalExamPreparation/secretChat.js
+++ js-fundamentals/FinalExamPreparation/secretChat.js
@@ -9,7 +9,8 @@ function secretChat(input) {
         switch (command) {
             case 'ChangeAll':
                 while (message.includes(token1)) {
-                    message = message.replace(token1, token2);
+                    let regex = new RegExp(token1, 'g');
+                    message = message.replace(regex, token2);
                 }
                 console.log(message);
                 break;
@@ -62,35 +63,3 @@ secretChat([
 
     'Reveal',
 ]);
-
-// function revealMessage(inputArr) {
-//     let message = inputArr.shift();
-//     let currentCommand = inputArr.shift();
-
-//     while (currentCommand !== "Reveal") {
-//       let [command, arg1, arg2] = currentCommand.split(":|:");
-
-//       if (command === "InsertSpace") {
-//         message = message.slice(0, arg1) + " " + message.slice(arg1);
-//         console.log(message);
-//       } else if (command === "Reverse") {
-//         if (message.includes(arg1)) {
-//           let reversedSubstring = arg1.split("").reverse().join("");
-//           message = message.replace(arg1, "");
-//           message += reversedSubstring;
-//           console.log(message);
-//         } else {
-//           console.log("error");
-//         }
-//       } else if (command === "ChangeAll") {
-//         while (message.includes(arg1)) {
-//           message = message.replace(arg1, arg2);
-//         }
-//         console.log(message);
-//       }
-
-//       currentCommand = inputArr.shift();
-//     }
-
-//     console.log(`You have a new text message: ${message}`);
-//   }
