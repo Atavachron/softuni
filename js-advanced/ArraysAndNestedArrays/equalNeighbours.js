ed7463149f39f10cd272d35ec7c92b6436426f66
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-05-17 01:00:51 +0400

    Check a matrix for equal neighbors

---
+++ js-advanced/ArraysAndNestedArrays/equalNeighbours.js
@@ -0,0 +1,22 @@
+function solve(matrix) {
+    let count = 0;
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (j < matrix[i].length - 1 && matrix[i][j] === matrix[i][j + 1]) {
+                count++;
+            }
+            if (i < matrix.length - 1 && matrix[i][j] === matrix[i + 1][j]) {
+                count++;
+            }
+        }
+    }
+    console.log(count);
+}
+
+solve([
+    ['2', '3', '4', '7', '0'],
+    ['4', '0', '5', '3', '4'],
+    ['2', '3', '5', '4', '2'],
+    ['9', '8', '7', '5', '4'],
+]);
