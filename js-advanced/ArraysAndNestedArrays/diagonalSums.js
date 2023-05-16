b4c89d282963b2fd6b9b00190a32bcec73f3dc70
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-05-17 00:25:52 +0400

    Find the sum of main and secondary diagonals of a matrix

---
+++ js-advanced/ArraysAndNestedArrays/diagonalSums.js
@@ -0,0 +1,28 @@
+// A square matrix of numbers comes as an array of arrays, each array holding numbers. Write a function that finds the
+// sum at the main and the secondary diagonals.
+// The input comes as an array of arrays, containing number elements (2D matrix of numbers).
+// The output is printed on the console, on a single line separated by space. First print the sum at the main diagonal,
+// then the sum at the secondary diagonal.
+
+function solve(matrix) {
+    let mainSum = 0;
+    let secondarySum = 0;
+
+    for (let i = 0; i < matrix.length; i++) {
+        mainSum += matrix[i][i];
+        secondarySum += matrix[i][matrix.length - 1 - i];
+    }
+
+    console.log(`${mainSum} ${secondarySum}`);
+}
+
+solve([
+    [20, 40],
+    [10, 60],
+]);
+
+solve([
+    [3, 5, 17],
+    [-1, 7, 14],
+    [1, -8, 89],
+]);
