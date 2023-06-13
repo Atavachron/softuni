be25a4117e5f7a878f0ffde9069059f736a51386
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-06-13 18:54:36 +0400

    Sum the numbers in the last column of a table

--- js-advanced/DOMIntroduction/sumTable/SumTable.js
+++ js-advanced/DOMIntroduction/sumTable/SumTable.js
@@ -1,3 +1,17 @@
 function sumTable() {
+    let table = document.getElementsByTagName('table')[0];
+    let rows = table.querySelectorAll('tr');
+    let rowsArr = Array.from(rows);
+    let resultEl = document.getElementById('sum');
+    let sum = 0;
 
+    for (let i = 1; i < rowsArr.length - 1; i++) {
+        let row = rowsArr[i];
+
+        let td = row.querySelector('td:nth-child(2)');
+
+        sum += Number(td.textContent);
+    }
+
+    resultEl.textContent = sum;
 }
