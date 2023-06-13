e4f2fc650008272125d285ad8020da203a5d99f0
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-06-13 17:52:04 +0400

    Scan list element and add text content to text area

--- js-advanced/DOMIntroduction/collectList/CollectListItems.js
+++ js-advanced/DOMIntroduction/collectList/CollectListItems.js
@@ -1,3 +1,9 @@
 function extractText() {
-    // TODO
+    let list = document.getElementsByTagName('li');
+    let result = document.getElementById('result');
+
+    let listArray = Array.from(list);
+    let textArray = listArray.map(li => li.textContent);
+
+    result.value = textArray.join('\n');
 }
