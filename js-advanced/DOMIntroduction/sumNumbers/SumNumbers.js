99d137c203fd82bdf47b042d6029c870bebc7c96
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-06-13 18:09:32 +0400

    Read input elements value and print calculation in third field

--- js-advanced/DOMIntroduction/sumNumbers/SumNumbers.js
+++ js-advanced/DOMIntroduction/sumNumbers/SumNumbers.js
@@ -1,3 +1,12 @@
 function calc() {
     // TODO: sum = num1 + num2
+    let input1El = document.getElementById('num1');
+    let input2El = document.getElementById('num2');
+    let sumEl = document.getElementById('sum');
+
+    let num1 = Number(input1El.value);
+    let num2 = Number(input2El.value);
+    let result = num1 + num2;
+
+    sumEl.value = result;
 }
