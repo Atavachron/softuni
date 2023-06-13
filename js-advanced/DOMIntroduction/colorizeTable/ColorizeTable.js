1360c5fcc7de6237c6b77d56b2f37a2c8c14f9ed
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-06-13 18:37:04 +0400

    Colorize every even row in a column

--- js-advanced/DOMIntroduction/colorizeTable/ColorizeTable.js
+++ js-advanced/DOMIntroduction/colorizeTable/ColorizeTable.js
@@ -1,3 +1,10 @@
 function colorize() {
-    // TODO
+    let table = document.querySelector('table');
+    let rows = table.getElementsByTagName('tr');
+
+    for (let i = 0; i < rows.length; i++) {
+        if (i % 2 !== 0) {
+            rows[i].style.backgroundColor = 'teal';
+        }
+    }
 }
