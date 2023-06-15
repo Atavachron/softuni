694273076327f487c06fd8845ac20a79a5ea80e2
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-06-15 18:22:26 +0400

    Append options to drop down select menu

--- js-advanced/DOMManipulationsExercises/Fill-Dropdown/dropdown.js
+++ js-advanced/DOMManipulationsExercises/Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    let inputText = document.getElementById('newItemText').value;
+    let inputValue = document.getElementById('newItemValue').value;
+
+    let selectEl = document.getElementById('menu');
+    let option = document.createElement('option');
+
+    option.textContent = inputText;
+    option.value = inputValue;
+
+    selectEl.appendChild(option);
+    document.getElementById('newItemText').value = '';
+    document.getElementById('newItemValue').value = '';
 }
