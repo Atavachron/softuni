f69bded2f3b0aadcca9f8ff169244a95b0afb6ff
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-06-15 13:56:51 +0400

    Search text in table and change background color

--- js-advanced/DomIntroductionExercises/tableSearchEngine/solution.js
+++ js-advanced/DomIntroductionExercises/tableSearchEngine/solution.js
@@ -1,8 +1,19 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+    document.querySelector('#searchBtn').addEventListener('click', onClick);
+    let table = document.querySelector('.container');
+    let rows = table.querySelectorAll('tbody tr');
 
-   function onClick() {
-      //   TODO:
+    function onClick() {
+        let searchValue = document
+            .getElementById('searchField')
+            .value.toLowerCase();
 
-   }
+        for (let row of rows) {
+            if (row.textContent.toLowerCase().includes(searchValue)) {
+                row.className = 'select';
+            } else {
+                row.className = '';
+            }
+        }
+    }
 }
