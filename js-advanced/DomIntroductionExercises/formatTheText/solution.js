5172682510359f3d6ce60e79b3e60eb1e7d086ab
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-06-15 14:30:12 +0400

    Format input sentences into paragraphs of three

--- js-advanced/DomIntroductionExercises/formatTheText/solution.js
+++ js-advanced/DomIntroductionExercises/formatTheText/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODO
+    let inputEl = document.getElementById('input');
+    let outputEl = document.getElementById('output');
+
+    let text = inputEl.value;
+    let sentencesArray = text
+        .split('.')
+        .filter(s => s !== '')
+        .map(s => s + '.');
+    let paragraphCount = Math.ceil(sentencesArray.length / 3);
+
+    for (let i = 0; i < paragraphCount; i++) {
+        let p = document.createElement('p');
+        let joined = sentencesArray.splice(0, 3).join('');
+        p.textContent = joined;
+        outputEl.appendChild(p);
+    }
 }
