7308fb39ed5dc0764b9263c97cb61c203b8e2099
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-06-15 13:05:55 +0400

    Display and hide text with the click of a button

--- js-advanced/DomIntroductionExercises/accordion/accordion.js
+++ js-advanced/DomIntroductionExercises/accordion/accordion.js
@@ -1,3 +1,8 @@
 function toggle() {
-    console.log('TODO:...');
+    let btn = document.querySelector('.button');
+    let text = document.querySelector('#extra');
+
+    btn.textContent = btn.textContent === 'More' ? 'Less' : 'More';
+    text.style.display =
+        text.style.display === 'none' || !text.style.display ? 'block' : 'none';
 }
