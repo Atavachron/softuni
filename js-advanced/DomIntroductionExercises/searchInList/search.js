fe4bb85d98f9e2507b50e9344f3319633f1c233a
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-06-15 13:22:26 +0400

    Search in list and format result

--- js-advanced/DomIntroductionExercises/searchInList/search.js
+++ js-advanced/DomIntroductionExercises/searchInList/search.js
@@ -1,3 +1,20 @@
 function search() {
-   // TODO
+    let towns = Array.from(document.querySelectorAll('#towns li'));
+    let searchValue = document.getElementById('searchText').value.toLowerCase();
+    let result = document.getElementById('result');
+    let matches = 0;
+    result.textContent = '';
+
+    towns.forEach(town => {
+        town.style.fontWeight = 'normal';
+        town.style.textDecoration = 'none';
+
+        if (town.textContent.toLowerCase().includes(searchValue)) {
+            town.style.fontWeight = 'bold';
+            town.style.textDecoration = 'underline';
+            matches++;
+        }
+    });
+
+    result.textContent = `${matches} matches found`;
 }
