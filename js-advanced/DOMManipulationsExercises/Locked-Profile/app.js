965910aa042677ed48e833fb2f6899f4ec520467
Atavachron <37329317+Atavachron@users.noreply.github.com>
2023-06-15 18:11:37 +0400

    Show and hide special info if profile is unlocked

--- js-advanced/DOMManipulationsExercises/Locked-Profile/app.js
+++ js-advanced/DOMManipulationsExercises/Locked-Profile/app.js
@@ -1,3 +1,24 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const buttons = Array.from(document.querySelectorAll('.profile > button'));
+
+    buttons.forEach(button => button.addEventListener('click', toggleInfo));
+
+    function toggleInfo(e) {
+        //check if the profile is locked
+        let isLocked =
+            e.target.parentElement.querySelector('[value="lock"]').checked;
+
+        //access hidden area
+        let hiddenInfo = e.target.previousElementSibling;
+
+        if (isLocked) return;
+
+        if (e.target.textContent === 'Show more') {
+            hiddenInfo.style.display = 'block';
+            e.target.textContent = 'Hide it';
+        } else {
+            hiddenInfo.style.display = 'none';
+            e.target.textContent = 'Show more';
+        }
+    }
 }
